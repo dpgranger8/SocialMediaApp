7763ce9199cb07e1544a2264f78a22045811dcfe
David Granger <dpgranger8@gmail.com>
2023-07-22 16:51:29 -0600

    edited .gitignore

---
+++ techSocialMediaApp/Models/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  techSocialMediaApp
+//
+//  Created by David Granger on 7/22/23.
+//
+
+import Foundation
+
+struct Constants {
+    static let username: String = "DAVID.GRANGER0790@STU.MTEC.EDU"
+    static let password: String = "8e45ea49-284a-4930-a250-a28dc445b4d2"
+}
